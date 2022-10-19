0098ea500e36628e118e8a727ae53dd6bf50d94a
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-19 17:31:29 +0530

    Added Initial Project Dependencies

---
+++ src/main/java/com/UserRegistration/App.java
@@ -0,0 +1,16 @@
+package com.UserRegistration;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Regex Practise Problem");
+    }
+}
